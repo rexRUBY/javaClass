0b21c7ef24a3f417a08fc99cf97879e75d25c227
Yeseo Park <87340601+rexRUBY@users.noreply.github.com>
2022-06-02 14:50:21 +0900

    Update GoodMorining.java

--- Calendar/GoodMorining.java
+++ Calendar/GoodMorining.java
@@ -6,6 +6,10 @@ public class GoodMorning {
 	public static void main(String[] args) {
 		Calendar now = Calendar.getInstance();
 		
+		int hourOfDay = now.get(Calendar.HOUR_OF_DAY);
+		int minute = now.get(Calendar.MINUTE);
+		System.out.println("현재 시간은 " + hourOfDay + "시 " + minute + "분입니다.");
+		
 		if(now.get(Calendar.HOUR_OF_DAY)>4 && now.get(Calendar.HOUR_OF_DAY)<12)
 			System.out.println("Good Morning");
 		else if(now.get(Calendar.HOUR_OF_DAY)<18 && now.get(Calendar.HOUR_OF_DAY)>12)
@@ -14,7 +18,6 @@ public class GoodMorning {
 			System.out.println("Good Evening");
 		else
 			System.out.println("Good Night");
-			
 	}
-
 }
+
