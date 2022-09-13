f31abd0746ecc5d36509a5e90e7c2b610256f555
Yeseo Park <87340601+rexRUBY@users.noreply.github.com>
2022-09-13 23:48:15 +0900

    1

---
+++ ch08/ContentPaneEx.java
@@ -0,0 +1,24 @@
+package javaClass;
+import javax.swing.*;
+import java.awt.*;
+
+public class ContentPaneEx extends JFrame {
+	public ContentPaneEx() {
+		setTitle("ContentPane과 JFrame의 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container contentPane = getContentPane();
+		contentPane.setBackground(Color.ORANGE);
+		contentPane.setLayout(new FlowLayout());
+		
+		contentPane.add(new JButton("OK"));
+		contentPane.add(new JButton("Cancel"));
+		contentPane.add(new JButton("Ignore"));
+		
+		setSize(300, 150);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new ContentPaneEx();
+	}
+}
